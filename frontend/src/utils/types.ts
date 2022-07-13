63f5ae09252be59919a0f1853138d4d4bfa0c91b
Fellipe Araujo <fellipe.eng.soft@gmail.com>
2022-07-12 23:07:12 -0300

    #1 create news interfaces

--- frontend/src/utils/types.ts
+++ frontend/src/utils/types.ts
@@ -1,6 +1,18 @@
-import React from "react";
+import React from 'react';
 
 export interface OptionProps {
   icon: React.ReactElement;
   title: string;
+  active?: boolean;
+  onClick?(): void;
+}
+
+export interface ModalConfigSessionProps {
+  isModalOpen: boolean;
+  closeModal(): void;
+}
+
+export interface ButtonEditParamProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  icon: React.ReactElement;
 }
