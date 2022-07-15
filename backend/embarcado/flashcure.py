490b98948f76e47e6367db4f98310fefce876072
Sam Buters <sambuters@gmail.com>
2022-07-14 22:38:06 -0300

    Adding FlashcureController

---
+++ backend/embarcado/flashcure.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+
+def celsius2Percentage(celsius, limits):
+	if(celsius > limits[1] or celsius < limits[0]):
+		raise ValueError("Temperature provided is not within the defined limits.")
+
+	# Linear
+	return celsius/limits[1]
+
+class FlashcureController:
+	def __init__(self, celsius, channel=12, frequency=20):
+		self.temperature = celsius2Percentage(celsius) * 100
+
+		GPIO.setmode(GPIO.BOARD)
+		GPIO.setup(channel, GPIO.OUT)
+
+		self.pwm = GPIO.PWM(channel, frequency)
+
+	def __del__(self):
+		self.stop()
+		GPIO.cleanup()
+
+	def start(self):
+		self.pwm.start(self.temperature)
+
+	def stop(self):
+		self.pwm.stop()
+
+	def setSpeed(self, celsius):
+		self.temperature = celsius2Percentage(celsius) * 100
