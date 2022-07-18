18393ebbceb83dab85dd074eb8e880c0d6cb3f32
Fellipe Araujo <fellipe.eng.soft@gmail.com>
2022-07-18 15:13:48 -0300

    #10 refactor option component

--- frontend/src/components/Option.tsx
+++ frontend/src/components/Option.tsx
@@ -1,12 +1,31 @@
+import { Link } from 'react-router-dom';
 import { OptionProps } from '../utils/types';
 
-import '../styles/components/Option.scss'
+import '../styles/components/Option.scss';
 
 export function Option(props: OptionProps) {
   return (
     <button id="option" onClick={props.onClick}>
       {props.icon}
-      <h2 className={props.active ? 'session-active' : ''}>{props.title}</h2>
+      {props.route === '/quality' ? (
+        <h2
+          className={
+            props.active ? 'session-active session-title' : 'session-title'
+          }
+        >
+          {props.title}
+        </h2>
+      ) : (
+        <Link to={props.route!} style={{ textDecoration: 'none' }}>
+          <h2
+            className={
+              props.active ? 'session-active session-title' : 'session-title'
+            }
+          >
+            {props.title}
+          </h2>
+        </Link>
+      )}
     </button>
-  )
+  );
 }
