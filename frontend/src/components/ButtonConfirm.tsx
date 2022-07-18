308d225ded73e89389118607e07c64a08a751fc1
Fellipe Araujo <fellipe.eng.soft@gmail.com>
2022-07-18 15:08:32 -0300

    #10 refactor button confirm component

--- frontend/src/components/ButtonConfirm.tsx
+++ frontend/src/components/ButtonConfirm.tsx
@@ -2,9 +2,13 @@ import { ButtonConfirmProps } from '../utils/types';
 
 import '../styles/components/ButtonConfirm.scss';
 
-export function ButtonConfirm({ title, ...rest }: ButtonConfirmProps) {
+export function ButtonConfirm({ title, color, ...rest }: ButtonConfirmProps) {
   return (
-    <button id="button-confirm" {...rest}>
+    <button
+      id="button-confirm"
+      style={{ backgroundColor: `${color}` }}
+      {...rest}
+    >
       <h3>{title}</h3>
     </button>
   );
