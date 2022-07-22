3a08a1d0accc49f103a49618d7cc420dd6711e91
Sam Buters <sambuters@gmail.com>
2022-07-21 22:30:31 -0300

    Adding paint to dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { ToastContainer } from 'react-toastify';
 
 import { SideBar } from '../components/SideBar';
@@ -6,6 +8,39 @@ import '../styles/pages/Dashboard.scss';
 import 'react-toastify/dist/ReactToastify.css';
 
 export function Dashboard() {
+  const PAINT_TYPES = [
+    'Água', 'Plastisol'
+  ];
+
+  const SESSION_PARAMETERS = {
+    paints: [
+      {
+        type: 0,
+        color: 'Vermelho'
+      },
+
+      {
+        type: 1,
+        color: 'Azul'
+      },
+
+      {
+        type: 0,
+        color: 'Amarelo Pateta'
+      }
+    ],
+    batches: 4
+  };
+
+  const CURRENT_STATE = {
+    paint: 0,
+    batch: 0,
+    speed: 25,
+    temperature: 150
+  }
+
+  const [currentPaint, setCurrentPaint] = useState(0);
+
   return (
     <div id="dashboard">
       <ToastContainer />
@@ -13,13 +48,22 @@ export function Dashboard() {
       <SideBar />
 
       <div class="main">
-        <h2>Dashboard</h2>
-        
+        <h1>Dashboard</h1>
+
         <div class="content">
-          <div class="temperatura"></div>
-          <div class="qualidade"></div>
-          <div class="tinta"></div>
-          <div class="tempo"></div>
+          <div class="temperature"></div>
+          <div class="quality"></div>
+          <div class="speed"></div>
+          <div class="paint">
+            <h1>Tinta</h1>
+
+            <div class="info">
+              <h2>{ SESSION_PARAMETERS.paints[CURRENT_STATE.paint].color }</h2>
+              <h3>{ PAINT_TYPES[SESSION_PARAMETERS.paints[CURRENT_STATE.paint].type] }</h3>
+              <h4>{CURRENT_STATE.paint + 1}/{SESSION_PARAMETERS.paints.length}</h4>
+            </div>
+          </div>
+          <div class="time"></div>
         </div>
       </div>
     </div>
