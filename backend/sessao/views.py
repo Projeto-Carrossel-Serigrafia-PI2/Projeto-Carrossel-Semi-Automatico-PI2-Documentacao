b79d42aa0d153bc52e560ae40007aea3974be27e
Sam Buters <sambuters@gmail.com>
2022-07-26 16:47:03 -0300

    [WIP] Adding production start and handlers

--- backend/sessao/views.py
+++ backend/sessao/views.py
@@ -1,15 +1,28 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-#from carrossel.settings import CONFIG
+# from carrossel.settings import CONFIG
 
 from sessao.models import Producao, Base, BaseProducao, Lote
 
+import asyncio
+import keyboard # For debugging
 import pprint
 
-#MOTOR_STEP = CONFIG['ENCODER_HOLES']/4
+CONFIG = {
+	'FLASHCURE': {
+		'POSITION': 2
+	},
+	'CAMERA': {
+		'POSITION': 3
+	},
+	'ENCODER_HOLES': 4,
+}
 
-#motorController = None
+MOTOR_STEP = CONFIG['ENCODER_HOLES']/4
+
+# motorController = MotorController.instance(0)
+# flashcureController = FlashcureController.instance(0)
 
 PAINTS = list(Base.objects.all())
 
@@ -21,6 +34,7 @@ state = {
 		'speed': 2,
 	},
 	'paint': 0,
+	'rotation': 0,
 	'batch': 0,
 	'shirt': 0,
 	'temperature': 100,
@@ -28,22 +42,60 @@ state = {
 	'encoderCounter': 0,
 }
 
-# def cleanCallbacks():
-# 	GPIO.remove_event_detect(CONFIG['PIN']['PEDAL'])
-# 	GPIO.remove_event_detect(CONFIG['PIN']['ENCODER'])
+async def dryShirt():
+	currentPaint = PAINTS[state['paints'][state['paint']].type]
 
-# def pedalHandler(channel):
-# 	if motorController.isRotating is False:
-# 		motorController.start()
+	# flashcureController.start()
+	print('Flashcure is on!')
+	await asyncio.sleep(currentPaint.tempoSecagem)
+	# flashcureController.stop()
+	print('Flashcure is off!')
 
-# def encoderHandler(channel):
-# 	global encoderCounter
+def pedalHandler(channel):
+	if(inSession and is not motorController.isRotating):
+		# flashcureController.stop()
+		# motorController.start()
+		print('Pedal pressed & motor started!')
 
-# 	encoderCounter += 1
+def encoderHandler(channel):
+	state['encoderCounter'] += 1
 
-# 	if(encoderCounter >= MOTOR_STEP):
-# 		motorController.stop()
-# 		encoderCounter = 0
+	if(state['encoderCounter'] >= MOTOR_STEP):
+		# motorController.stop()
+		print('Motor stopped!')
+
+		state['rotation'] += 1
+
+		# If the first shirt with the newest color has arrived at the flashcure position
+		if(state['rotation'] == CONFIG['FLASHCURE']['POSITION']):
+			currentPaint = PAINTS[state['paints'][state['paint']].type]
+			# flashcureController.setTemperature(currentPaint.temperaturaSecagem)
+			print('Changed temperature!')
+
+		# If a shirt with the newest color has arrived at the flashcure position
+		if(state['paint'] > 0 or state['rotation'] >= CONFIG['FLASHCURE']['POSITION']):
+			asyncio.run(dryShirt())
+
+		# If we completed a full rotation
+		if(state['rotation'] > 3):
+			state['rotation'] = 0
+			state['paint'] += 1
+
+			# If we went through all the paints (doesn't mean they have been dried yet)
+			if(state['paint'] >= len(state['parameters']['paints'])):
+				state['paint'] = 0
+				state['batch'] += 1
+
+				# If we went through all the batches (doesn't mean they have been dried yet)
+				if(state['batch'] >= len(state['parameters']['batches'])):
+					state['inSession'] = False
+
+		state['encoderCounter'] = 0
+
+#	GPIO.add_event_detect(CONFIG['PIN']['PEDAL'], GPIO.FALLING, callback=pedalHandler, bouncetime=50)
+#	GPIO.add_event_detect(CONFIG['PIN']['ENCODER'], GPIO.RISING, callback=encoderHandler, bouncetime=25)
+keyboard.add_hotkey('ctrl', pedalHandler, args=(3))
+keyboard.add_hotkey('shift', encoderHandler, args=(7))
 
 def setupProduction():
 	production = Producao.objects.last()
@@ -59,6 +111,7 @@ def setupProduction():
 	state['batch'] = 0
 	state['shirt'] = 0
 	state['encoderCounter'] = 0
+	state['rotation'] = 0
 	
 	state['inSession'] = True
 
@@ -68,12 +121,14 @@ def setupProduction():
 	#pprint.pprint(len(batches))
 
 def startProduction():
-	pass
+	currentPaint = PAINTS[state['paints'][state['paint']].type]
+	# motorController = MotorController.instance(state['parameters']['speed'])
+	# flashcureController = FlashcureController.instance(currentPaint.temperaturaSecagem)
 
 class ControleProducaoView(APIView):
 	def post(self, request):
 		action = request.data['action'] # 0: Start, 1: Stop, ...
-		if action == 0:
+		if(action == 0):
 			setupProduction()
 			startProduction()
 
