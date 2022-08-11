981b7f73d525d28f932eb1fa6dc8fc16bd3b4126
Fellipe Araujo <fellipe.eng.soft@gmail.com>
2022-08-11 19:24:39 -0300

    #17 remove update function from lote

--- backend/sessao/api/serializers.py
+++ backend/sessao/api/serializers.py
@@ -40,12 +40,3 @@ class LoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Lote
         fields = '__all__'           
-
-    def update(self, instance, validated_data):
-        instance.image = validated_data.get('image', instance.image)
-        ##implementar aqui a validação da camiseta
-        instance.aprovado = 'n'
-        instance.save()
-        return instance
-       
-    
