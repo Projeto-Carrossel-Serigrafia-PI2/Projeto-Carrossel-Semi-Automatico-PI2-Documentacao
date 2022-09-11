9bd6cbde8fbadb5fc94919da1a3406cbcaded1bd
ErickGiffoni <giffoni.erick@gmail.com>
2022-09-11 17:47:18 -0300

    update disable hotspot script

--- disable-hotspot.sh
+++ disable-hotspot.sh
@@ -3,6 +3,6 @@
 sudo systemctl stop hostapd
 
 echo "edit /etc/dhcpcd.conf ==> comment lines related to static ip address"
-echo
+echo "run: sudo systemctl restart dhcpcd"
 echo "[x] now reboot ==> sudo reboot"
 
