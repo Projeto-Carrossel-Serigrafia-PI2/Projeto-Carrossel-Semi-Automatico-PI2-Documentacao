2d2372759ed8c7d34c29600ec42fce0884ffdbcf
ErickGiffoni <giffoni.erick@gmail.com>
2022-09-11 19:06:17 -0300

    update hotspot instructions

--- start-hotspot.sh
+++ start-hotspot.sh
@@ -4,6 +4,7 @@ sudo systemctl stop dnsmasq
 sudo systemctl stop hostapd
 
 echo "edit /etc/dhcpcd.conf ==> static ip address"
+echo "put there => interface wlan0 /n static ip_address=192.168.0.10/24 /n nohook wpa_supplicant"
 echo "sudo service dhcpcd restart"
 echo "sudo systemctl start dnsmasq"
 echo "sudo systemctl unmask hostapd"
