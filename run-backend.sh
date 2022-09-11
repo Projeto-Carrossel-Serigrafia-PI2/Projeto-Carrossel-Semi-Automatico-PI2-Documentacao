a0440b6c6e8f031a01920016a74b1baa5576b17c
ErickGiffoni <giffoni.erick@gmail.com>
2022-09-11 17:52:15 -0300

    make backend server accessible by anyone

--- run-backend.sh
+++ run-backend.sh
@@ -2,5 +2,5 @@
 
 cd /home/pi/pi2/Projeto-Carrossel-Semi-Automatico-PI2-Documentacao/backend/ 
 source .venv39/bin/activate
-sudo python3.9 manage.py runserver &
+sudo python3.9 manage.py runserver 0.0.0.0:8000 &
 
