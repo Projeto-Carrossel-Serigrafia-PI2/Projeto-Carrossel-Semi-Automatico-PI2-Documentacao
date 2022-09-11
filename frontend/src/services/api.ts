2ec7cb74ff997e4fb6689faa2b8e7da67984a45c
ErickGiffoni <giffoni.erick@gmail.com>
2022-09-11 19:05:33 -0300

    use backend IP address based on host IP

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,18 @@
 import axios from 'axios';
 
+function getBaseUrl(){
+  let host = window.location.hostname;
+  if(host !== 'localhost'){
+    host = '192.168.0.10';
+  }
+
+  let baseUrl = 'http://' + host + ':8000';
+  
+  console.log('host is ' + host);
+  
+  return baseUrl;
+}
+
 export const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: getBaseUrl(),
 });
